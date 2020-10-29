3cdd34f97e201429d57fe93802848389026ab26d
蔡范宾 <745604472@qq.com>
2020-10-29 11:12:50 +0800

    工具类

--- src/main/java/com/binge/utils/util/StringUtil.java
+++ src/main/java/com/binge/utils/util/StringUtil.java
@@ -1,5 +1,10 @@
 package com.binge.utils.util;
 
+import com.alibaba.fastjson.JSON;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public class StringUtil {
@@ -30,4 +35,23 @@ public class StringUtil {
         return UUID.randomUUID().toString().replace("-","");
     }
 
+    /**
+     * 占位符替换
+     * @param text
+     * @param param
+     * @return
+     */
+    public static String  parse(String text,String param){
+        if(StringUtils.isBlank(param)){
+            return text;
+        }
+        String start = "${";
+        String end = "}";
+        Map<String,String> map = JSON.parseObject(param, Map.class);
+        Set<String> keys = map.keySet();
+        for (String key: keys) {
+            text = text.replace(start+key+end, map.get(key));
+        }
+        return text;
+    }
 }
